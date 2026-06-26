6eb80927ce4a6e93ef7de4d56b486362f521da8c
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-06-26 23:50:33 +0530

    feat: implement user authentication using Better Auth with Prisma and GitHub provider

---
+++ utils/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { prisma } from "../lib/db"
+
+import {nextCookies} from "better-auth/next-js"
+
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    emailAndPassword:{
+        enabled:true,
+    },
+    socialProviders : {
+        github : {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }
+    },
+    plugins : [nextCookies()]
+    
+}); 
